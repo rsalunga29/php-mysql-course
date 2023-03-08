5552c24f34de0a482da1ee1a6504fcaf21f6d7d2
Akio <akio@pop-os.localdomain>
2023-03-08 20:32:08 +0800

    initial commit

---
+++ basic_syntax/hello.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "Hello, World!";
+
+echo "<br>";
+
+print "<br>";
+
+print "Welcome to PHP & MySQL Course!";
---
+++ variable_constants/arrays.php
@@ -0,0 +1,5 @@
+<?php
+
+$whiskeys = array('Jack Daniels', 'Black Label', 'Jim Beam', 'Hennessy', 'Jameson');
+
+$beers = ['Red Horse', 'Pale Pilsen', 'Heineken', 'Corona', 'Bud Light', 'Budweiser'];
